a81baf46d13c2dae49e652c52cb345f1b6bb5e1b
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2019-04-12 15:33:37 -0400

    feat: Add instances and member functions

--- include/searchEngine.h
+++ include/searchEngine.h
@@ -1,3 +1,5 @@
+#include <vector>
+
 #ifndef SEARCHENGINE_H
 #define SEARCHENGINE_H
 
@@ -5,12 +7,21 @@
 class searchEngine
 {
     public:
-        searchEngine();
+        searchEngine(bool d, bool f, bool v);
         virtual ~searchEngine();
 
     protected:
 
     private:
+        struct
+        {
+            bool directory, file, verbose;
+        }
+        options;
+
+        char* currentDir;
+        int contCol, contRow, currentRows;
+        std::vector<int> point;
 };
 
 #endif // SEARCHENGINE_H
